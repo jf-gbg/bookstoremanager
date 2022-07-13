c1c735e999f4d01a78de4c9fddf9bd76e2013624
Dev <Dev@MacBook-Pro.local>
2022-07-13 17:04:04 +0200

    complete an exercise from the book

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int sum = 0;
+    int val = 50;
+
+    while(val <= 100) {
+        sum += val;
+        ++val;
+    }
+    std::cout << "Sum of 50 to 100 inclusive is " << sum << std::endl;
     return 0;
 }
