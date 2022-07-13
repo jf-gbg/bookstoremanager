8e42fec3717d0e4e0dfe082ad8ce50333c8eb3f0
Dev <Dev@MacBook-Pro.local>
2022-07-13 17:07:39 +0200

    complete exercise from book

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 
 int main() {
     int sum = 0;
-    int val = 50;
+    int val = 10;
 
-    while(val <= 100) {
-        sum += val;
-        ++val;
+    while(val >= 1) {
+        std::cout << val << std::endl;
+        --val;
     }
-    std::cout << "Sum of 50 to 100 inclusive is " << sum << std::endl;
+    //std::cout << "Sum of 50 to 100 inclusive is " << sum << std::endl;
     return 0;
 }
