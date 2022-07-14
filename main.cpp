cc564204fe10f5e5a475938c06ac786e04c678db
Dev <Dev@MacBook-Pro.local>
2022-07-14 08:52:09 +0200

    complete exercise from book

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Enter numbers seperated by a space. "
-                 "Enter 'd' when finished" << std::endl;
-    int sum{ 0 };
+    std::cout << "Enter 2 numbers seperated by space." << std::endl;
 
-    for(int value = 0; std::cin >> value;)
-        sum += value;
+    int userNum1 = 0;
+    int userNum2 = 0;
+    std::cin >> userNum1 >> userNum2;
 
-    std::cout << "Sum is: " << sum;
+    int least = 0;
+    int greatest = 0;
+
+    if(userNum1 < userNum2) {
+        least = userNum1;
+        greatest = userNum2;
+    } else {
+        least = userNum2;
+        greatest = userNum1;
+    }
+
+    std::cout << "Sum of numbers between " << least << " to " << greatest << " inclusive is..." << std::endl;
+
+    int sum = 0;
+    while(least <= greatest) {
+        sum += least;
+        ++least;
+    }
+
+    std::cout << sum << std::endl;
     return 0;
 }
