bfd0b0df47c1e3531c5eeb8aae46ad32c71be45e
jf-gbg <malmmlam@outlook.com>
2022-07-15 12:57:51 +0200

    books solution to problem

--- main.cpp
+++ main.cpp
@@ -11,22 +11,27 @@
 // of transactions. DONE
 //Write a program that adds up the total number of transactions for 2 sales items from a list
 // of transactions. - Limitations: result must be initialized if it is to show ISBN.DONE but fragile.
-//Write a program that can add up the total number of transactions for several sales items.
+//Write a program that can add up the total number of transactions for several sales items. Assuming
+// each item is grouped together we can use the same total counter and just update it to represent
+// a different item before writing to the console.
 int main() {
-    Sales_item transaction;
-    Sales_item result;
+    Sales_item total;
 
-    std::cin >> result;
-
-    while(std::cin >> transaction) {
-        if (transaction.isbn() == result.isbn()) {
-            result += transaction;
-        } else {
-            std::cout << result << std::endl;
-            result = transaction;
+    if (std::cin >> total) {
+        Sales_item trans;
+        while (std::cin >> trans) {
+            if (total.isbn() == trans.isbn())
+                total += trans;
+            else {
+                std::cout << total << std::endl;
+                total = trans;
+            }
         }
+        std::cout << total << std::endl;
+    } else {
+        std::cerr << "No data?!" << std::endl;
+        return -1;
     }
-    std::cout << result << std::endl;
 
     return 0;
 }
